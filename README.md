64b12efca0c0c9006bb50b1c7d3ea4028b4a4ef0
Kevin Booms <mrkbooms@gmail.com>
2023-04-17 18:45:42 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Data Structures & Algorithms
+
+## What's a Data Structure?
+A named location that can be used to store and organize data
+
+## What's an Algorithm
+A collection of steps to solve a problem
+
+## Why learn DS & A?
+1. You'll write code that is both time and memory efficient
+2. Commonly asked questions involve DS&A in coding job interviews
+
---
+++ src/main/java/Stacks.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Stacks {
+
+    public static Stack makeStack() {
+        Stack<String> stack = new Stack<String>();
+        stack.push("Mass Effect 2");
+        stack.push("Super Mario World");
+        stack.push("Earthbound");
+        stack.push("Ultima Online");
+        stack.push("World of Warcraft");
+        return stack;
+    }
+
+    public static void main(String[] args) {
+        /*
+         * Stack = LIFO (Last in-First out) data Structure
+         * stores objects into a sort of "vertical tower"
+         * push() to add to the top
+         * pop() to remove from the top
+         */
+        Stack<String> stack = new Stack<String>();
+        // this empty() method returns a boolean based on whether or not a stack is empty
+        System.out.println(stack.empty() + ", the stack is empty");
+
+        // now add items to the stack
+        stack = makeStack();
+
+        // empty() method should now return false as we have added data to the stack
+        System.out.println(stack.empty() + ", the stack has data");
+
+        // look at all the items in the stack
+        System.out.println("Size: " + stack.size() + " " + stack);
+
+        // how does the stack change as we pop each individual piece?
+        while (stack.size() > 0) {
+            System.out.println("Removing: " + stack.peek()); // use peek method to view the top piece of the stack
+            stack.pop();// remove the top
+            System.out.println("Size: " + stack.size() + " " + stack); // view the stack
+        }
+
+        // if we try to pop from an empty stack it will throw an error
+//        stack.pop();
+
+        stack = makeStack();
+        System.out.println("Reset Stack: " + stack);
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Position search | Enter a stack item: ");
+        String search = in.nextLine();
+        System.out.println(search + " location in stack: " + stack.search(search));
+    }
+}
